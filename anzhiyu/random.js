97c28da74470f45851ca8484c6d565d8130bd1eb
ruoshuiz.github.io <ruoshuiyun01@126.com>
2024-08-13 11:59:07 +0800

    Site updated: 2024-08-13 11:59:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/12/hello-world/"];function toRandomPost(){
+var posts=["2024/08/12/[Windows]洛雪音乐助手/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
