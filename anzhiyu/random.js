f3928af968568f4ceb470631b23045dc47dfc476
ruoshuiz <ruoshuiyun01@126.com>
2024-08-18 10:33:44 +0000

    github action update Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/13/Android-洛雪音乐助手/","2024/08/14/Windows-Bandizip/","2024/08/13/Windows-Bilidown/","2024/08/14/Windows-Lanzou_Electron/","2024/08/13/Windows-Ryujinx/","2024/08/12/Windows-洛雪音乐助手/","2024/08/18/新文章/"];function toRandomPost(){
+var posts=["2024/08/13/Android-洛雪音乐助手/","2024/08/14/Windows-Bandizip/","2024/08/13/Windows-Bilidown/","2024/08/14/Windows-Lanzou_Electron/","2024/08/13/Windows-Ryujinx/","2024/08/12/Windows-洛雪音乐助手/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
