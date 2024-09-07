2a4e9a7aa260ca1a65375ef5b09335620c31c920
ruoshuiz <ruoshuiyun01@126.com>
2024-09-07 09:31:28 +0000

    Publish Post source/_posts/Windows-洛雪音乐助手.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/pvz合集/","posts/A-BILIBILIAS/","posts/A-LSPatch/","posts/A-MT管理器/","posts/A-元力音乐/","posts/A-歌词适配/","posts/A-洛雪音乐/","posts/W-2345看图王/","posts/W-Bandizip/","posts/W-BetterGI/","posts/W-Bilidown/","posts/W-Lanzou_Electron/","posts/W-March7thAssistant/","posts/W-Ryujinx/","posts/W-洛雪音乐/"];function toRandomPost(){
+var posts=["posts/pvz合集/","posts/A-BILIBILIAS/","posts/A-LSPatch/","posts/A-MT管理器/","posts/A-元力音乐/","posts/A-歌词适配/","posts/A-洛雪音乐/","posts/W-2345看图王/","posts/W-Bandizip/","posts/W-BetterGI/","posts/W-Bilidown/","posts/W-Lanzou_Electron/","posts/W-March7thAssistant/","posts/W-Ryujinx/","posts/W-LXmusic/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
