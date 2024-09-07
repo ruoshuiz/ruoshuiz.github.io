268659ab1f6279b918f665b7f8a90790ba415ec8
ruoshuiz <ruoshuiyun01@126.com>
2024-09-07 09:20:10 +0000

    Publish Post source/_posts/Android-BILIBILIAS.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/pvz合集/","posts/586d04f6/","posts/1ff002b9/","posts/6cd45ac9/","posts/dba6fb02/","posts/3947d987/","posts/3f8b00fa/","posts/4e3c6833/","posts/5d2548b/","posts/1b7f1f1d/","posts/bae59f39/","posts/246872ec/","posts/dc74526/","posts/6041e474/","posts/c288664d/"];function toRandomPost(){
+var posts=["posts/pvz合集/","posts/A-BILIBILIAS/","posts/1ff002b9/","posts/6cd45ac9/","posts/dba6fb02/","posts/3947d987/","posts/3f8b00fa/","posts/4e3c6833/","posts/5d2548b/","posts/1b7f1f1d/","posts/bae59f39/","posts/246872ec/","posts/dc74526/","posts/6041e474/","posts/c288664d/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
